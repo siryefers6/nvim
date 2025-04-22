d48c57fe65c8c4c1ede8d0174cd9183c3b77abfd
yeferson <siryefers6@gmail.com>
2025-04-22 05:42:41 -0400

    feat: Agregar keymaps para cambiar de panel, configurar cmp para python

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,24 @@
+-- Guardar archivo con doble líder
 vim.keymap.set('n', '<leader><leader>w', ':w<cr>', {desc = "Guardar archivo" })
+
+-- Salir del archivo con doble líder
 vim.keymap.set('n', '<leader><leader>q', ':q<cr>', {desc = "Salir del archivo" })
 
-vim.keymap.set('', '<leader>rr', ':source %<cr>', {desc = "Refrescar configuración nvim" })
+-- Refrescar configuración de Neovim
+vim.keymap.set('', '<leader>rr', ':source %<cr>', {desc = "Refrescar configuración de Neovim" })
 
-vim.keymap.set('n', 'n', 'nzzzv', { desc = "Goes to the next result on the seach and put the cursor in the middle"})
-vim.keymap.set('n', 'N', 'Nzzzv', { desc = "Goes to the prev result on the seach and put the cursor in the middle"})
+-- Ir al siguiente resultado de la búsqueda y centrar el cursor
+vim.keymap.set('n', 'n', 'nzzzv', { desc = "Ir al siguiente resultado de la búsqueda y centrar el cursor" })
+
+-- Ir al resultado anterior de la búsqueda y centrar el cursor
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = "Ir al resultado anterior de la búsqueda y centrar el cursor" })
+
+-- Cambiar entre paneles creados con vsplit (izquierda)
+vim.keymap.set('n', '<leader><leader>h', '<C-w>h', { desc = "Cambiar al panel izquierdo" })
+
+-- Cambiar entre paneles creados con vsplit (derecha)
+vim.keymap.set('n', '<leader><leader>l', '<C-w>l', { desc = "Cambiar al panel derecho" })
+
+-- Volver al buffer anterior
+vim.keymap.set('n', '<leader><leader>b', ':buffer #<cr>', { desc = "Volver al buffer anterior" })
+
