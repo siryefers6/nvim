9d33c6bbb1b596820af5e58a5bfdf9deca46ce13
yeferson <siryefers6@gmail.com>
2025-04-29 00:47:08 +0000

    feat: Crear keymaps para copilot next y preview

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,16 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  config = function()
+    -- Keymaps para navegar sugerencias de Copilot
+    vim.keymap.set("i", "<F8>", "<Plug>(copilot-next)", {
+      silent = true,
+      desc = "Copilot: Sugerencia siguiente",
+    })
+
+    vim.keymap.set("i", "<F7>", "<Plug>(copilot-previous)", {
+      silent = true,
+      desc = "Copilot: Sugerencia anterior",
+    })
+  end,
 }
 
